54db84439420a1664e983f1531ba7696d0759a17
David von Hennigs <dvhennigs@gmail.com>
2023-08-10 16:04:25 +0200

    add toggle functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// phew… not a lot going on here. Please add some code!
+const buttonElement = document.querySelector('[data-js="button-element"]');
+
+buttonElement.addEventListener("click", () => {
+  buttonElement.classList.toggle("bookmark--active");
+});
+
+const answerButton = document.querySelector('[data-js="answer"]');
+const answer = document.querySelector('[data-js="show__answer"]');
+
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer");
+
+  if (answer.classList.contains("card__answer")) {
+    answerButton.innerText = "Show Answer";
+  } else {
+    answerButton.innerText = "Hide Answer";
+  }
+});
